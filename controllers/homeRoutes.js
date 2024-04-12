626dc2be0366fe195190854a2f22fe4273dd953b
Dave O <infodave212@gmail.com>
2024-04-11 23:12:45 -0500

    added the each division or sub-heading to the application placeholder, in the application.handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,10 +79,9 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 router.get('/apply', (req,res) =>{
-  if(req.session.logged_in){
+ 
     res.render('application')
-  }
-  res.render('login');
+
 })
 
 
